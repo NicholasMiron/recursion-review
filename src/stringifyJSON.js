929111a1ebb3696fe91508dd57624232336e4d05
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-20 08:55:53 -0600

    Implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var stringified = '';
+  if (typeof(obj) === 'number') { 
+    stringified += obj;
+  } else if (obj === null) {
+    stringified += null;
+  } else if (typeof(obj) === 'boolean') {
+    stringified += obj;
+  } else if (typeof(obj) === 'string') {
+    stringified += '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    if(obj.length === 0) {
+      stringified += '[]';
+    } else {
+      stringified += '[';
+      for(var i = 0; i < obj.length; i++) {
+        if(i === obj.length - 1) {
+          stringified += stringifyJSON(obj[i]);
+        } else {
+          stringified += stringifyJSON(obj[i]) + ',';
+        }
+      }
+      stringified += ']';
+    }
+  } else if(typeof(obj) === 'object') {
+    if(Object.keys(obj).length === 0) {
+      stringified += '{}';
+    } else {
+      stringified += '{';
+      var count = 0;
+      for(var key in obj) {
+        if(typeof(obj[key]) !== 'function' && obj[key] !== undefined) {
+          stringified += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+          if(count !== Object.keys(obj).length - 1) {              
+            stringified += ',';
+          }
+          count++;
+        }
+      }
+      stringified += '}';
+    }
+  }
+  return stringified;
 };
